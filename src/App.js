b0e72ca159bffe71d9b3605bae3dadda772f67e0
Luke Passey <ljpassey@gmail.com>
2022-12-19 13:48:57 -0700

    This commit includes parts 1 and 2 of my Unit-5 project which have been completed and contains a successfully established redux store and the framework for app-wide state management.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { useSelector } from "react-redux";
+
 import "./App.css";
 import Header from "./Components/Header";
 import MainDisplay from "./Components/MainDisplay";
 import OptionDisplay from "./Components/OptionDisplay";
+import { selectPotentials } from "./redux/slices/potentialCountriesSlice";
 
 function App() {
+    const potentials = useSelector(selectPotentials)
+    console.log(potentials)
     return (
         <div className="App font-link">
             <Header />
---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import  potentialCountriesReducer  from './slices/potentialCountriesSlice'
+
+export default configureStore({
+    reducer: {
+        potentialCountries: potentialCountriesReducer
+    }
+})
